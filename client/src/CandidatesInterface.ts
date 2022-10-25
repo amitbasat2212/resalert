612a971f488af5fdffe51687921f28f79cc02798
Or <orlevy11@gmail.com>
2022-10-25 11:39:10 +0300

    basic mvc #1

---
+++ client/src/CandidatesInterface.ts
@@ -0,0 +1,8 @@
+interface Candidate {
+    f_name:string,
+    l_name:string,
+    job: string,
+    status: string,
+    stage: string,
+    gender: string
+}
---
+++ client/src/Model.ts
@@ -0,0 +1,26 @@
+class Model{
+    static currentCandidates:Candidate[]
+    static currentJobs:Job[]
+
+    async logIn(userName:string, password:string){
+        $.get(`/autantication/login?userName=${userName}&password=${password}`)
+    }
+
+    async getCandidates(jobName:string, status:string, stage:string, gender:string): Promise<Candidate[]>{
+       Model.currentCandidates =  await $.get(`/candidates?job=${jobName}&status=${status}&stage=${stage}&gender=${gender}`)
+       return Model.currentCandidates
+    }
+
+    async getJobs(): Promise<Job[]>{
+        Model.currentJobs =  await $.get(`/jobs`)
+        return Model.currentJobs
+     }
+
+    async addNewCandidate(newCandidate: Candidate ): Promise<Candidate>{
+        return await $.post(`/candidates`,JSON.stringify(newCandidate))
+     }
+
+     async addNewJob(newJob: Job ): Promise<Job>{
+        return await $.post(`/jobs`,JSON.stringify(newJob))
+     }
+}
