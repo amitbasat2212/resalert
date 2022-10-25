34ffd53e5850cd37147c2af77b571e2de7df7614
edengil <edengil94@gmail.com>
2022-10-25 14:55:12 +0300

    init data and start the insertion of the tables

---
+++ SqlUtils/objects/departements.py
@@ -0,0 +1,15 @@
+class Departements:
+    def __init__(self, dep_name: str) -> None:
+        self.dep_name = dep_name
+
+    def __eq__(self, other):
+        return self.dep_name == other.dep_name
+
+    def __hash__(self):
+        return hash(self.dep_name)
+
+    def __str__(self) -> str:
+        return f"('{self.dep_name}')"
+
+    def __repr__(self) -> str:
+        return f"<Type>  {self.dep_name}"
---
+++ SqlUtils/utils/parser.py
@@ -0,0 +1,42 @@
+import json
+from objects.departements import Departements
+# from objects.trainer import Trainer
+# from objects.p_t_pair import P_T_Pair
+# from objects.type import Type
+# from utils.constants import *
+
+
+def _get_dict_from_json(json_dir: str):
+    person_jobs_data_file = open(json_dir)
+    person_jobs_data = json.load(person_jobs_data_file)
+    person_jobs_data_file.close()
+    return person_jobs_data
+
+
+
+def _parse_person_jobs_data(person_jobs_data):
+    # pokemons, pokemons_trainers, pokemons_types = set(), set(), set()
+
+    trainers = dict()  # {trainer: trainerId}
+    types = dict()  # {type: trainerId}
+    trainer_id, type_id = 1, 1
+    for p in person_jobs_data:
+        pokemon = Pokemon(p[P_ID], p[P_NAME], p[P_HEIGHT], p[P_WEIGHT])
+        pokemons.add(pokemon)
+        type = Type(type_id, p[P_TYPE])
+        if type not in types:
+            types[type] = type_id
+            type_id += 1
+        pokemons_types.add(P_T_Pair(pokemon.id, types[type]))
+        for t in p[P_TRAINER]:
+            trainer = Trainer(trainer_id, t[T_NAME], t[T_TOWN])
+            if trainer not in trainers:
+                trainers[trainer] = trainer_id
+                trainer_id += 1
+            pokemons_trainers.add(P_T_Pair(pokemon.id, trainers[trainer]))
+    return pokemons, trainers, pokemons_trainers, types, pokemons_types
+
+
+# def parse(json_dir: str):
+#     poke_data = _get_dict_from_json(json_dir)
+#     return _parse_poke_data(poke_data)
