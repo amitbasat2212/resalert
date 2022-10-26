25f5ff0985312d9a6ea70c93a88bfbb178833388
edengil <edengil94@gmail.com>
2022-10-26 15:55:35 +0300

    read_tables fixing

--- ServerUtils/Routes/AutanticationRoute.py
+++ ServerUtils/Routes/AutanticationRoute.py
@@ -3,14 +3,6 @@ from fastapi import APIRouter, Request, Form
 from fastapi.responses import FileResponse
 
 
-<<<<<<< HEAD
-router = APIRouter()
-
-
-@router.post('/login')
-async def login(username: str = Form(), password: str = Form()):
-    return FileResponse('./client/index.html')
-=======
 
 router = APIRouter()
 
@@ -18,7 +10,6 @@ router = APIRouter()
 def login(userName=None,password=None):
     return FileResponse('client/index.html')
 
->>>>>>> 93fb98e25d998279fa775f69fc6febacc968f1ec
 
 
 @router.get('/logout')
